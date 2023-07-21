29399567585a90cb8bed5241fd6064c54c4d2e4d
ThibaultAncele <t.ancele@net.estia.fr>
2023-07-21 19:31:32 +0200

    notion ngIf/For, interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,21 @@ import { Pokemon } from './pokemon';
 export class AppComponent implements OnInit {
 
   pokemonList: Pokemon[] = POKEMONS;
-  pokemon = POKEMONS[3];
+  pokemonSelected: Pokemon|undefined;
 
   ngOnInit() {
       console.table(this.pokemonList);
-      this.selectPokemon(this.pokemon);
   }
 
-  selectPokemon(pokemon: Pokemon){
-    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  selectPokemon(pokemonId: string){
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId);
+    if(pokemon) {
+      console.log(`Vous avez demandé le pokémon ${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    } else {
+      console.log(`Vous avez demandé un pokémon qui n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
+
   }
 }

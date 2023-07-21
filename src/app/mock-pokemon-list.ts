17d0723a2afb905751d9ed4584ae3858446d6a0c
ThibaultAncele <t.ancele@net.estia.fr>
2023-07-21 23:22:48 +0200

    Fin template,changement mock-pokemon,utilisation de materialize.css

--- src/app/mock-pokemon-list.ts
+++ src/app/mock-pokemon-list.ts
@@ -30,82 +30,82 @@ export const POKEMONS: Pokemon[] = [
     },
     {
         id: 4,
-        name: "Aspicot",
+        name: "Dracolosse",
         hp: 16,
         cp: 2,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/149.png",
         types: ["Insecte", "Poison"],
         created: new Date()
     },
     {
         id: 5,
-        name: "Roucool",
+        name: "Ectoplasma",
         hp: 30,
         cp: 7,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/094.png",
         types: ["Normal", "Vol"],
         created: new Date()
     },
     {
         id: 6,
-        name: "Rattata",
+        name: "Alakazam",
         hp: 18,
         cp: 6,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/065.png",
         types: ["Normal"],
         created: new Date()
     },
     {
         id: 7,
-        name: "Piafabec",
+        name: "Kyogre",
         hp: 14,
         cp: 5,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/382.png",
         types: ["Normal", "Vol"],
         created: new Date()
     },
     {
         id: 8,
-        name: "Abo",
+        name: "Groudon",
         hp: 16,
         cp: 4,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/023.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/383.png",
         types: ["Poison"],
         created: new Date()
     },
     {
         id: 9,
-        name: "Pikachu",
+        name: "Rayquaza",
         hp: 21,
         cp: 7,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/384.png",
         types: ["Electrik"],
         created: new Date()
     },
     {
         id: 10,
-        name: "Sabelette",
+        name: "Mew",
         hp: 19,
         cp: 3,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/151.png",
         types: ["Normal"],
         created: new Date()
     },
     {
         id: 11,
-        name: "Mélofée",
+        name: "Lugia",
         hp: 25,
         cp: 5,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/035.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/249.png",
         types: ["Fée"],
         created: new Date()
     },
     {
         id: 12,
-        name: "Groupix",
+        name: "Raichu",
         hp: 17,
         cp: 8,
-        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/037.png",
+        picture: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/026.png",
         types: ["Feu"],
         created: new Date()
     }

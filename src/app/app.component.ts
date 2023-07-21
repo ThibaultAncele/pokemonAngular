2886ac196c1984a35b389776b58f8ed4d9d675de
ThibaultAncele <t.ancele@net.estia.fr>
2023-07-21 09:01:25 +0200

    part Component and templateUrl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { POKEMONS } from './mock-pokemon-list';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Welcome to {{title}}!</h1>`
+  templateUrl:'app.component.html'
 })
-export class AppComponent {
-  title = 'ng-pokemon-app';
+export class AppComponent implements OnInit {
+
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemon = POKEMONS[3];
+
+  ngOnInit() {
+      console.table(this.pokemonList);
+      this.selectPokemon(this.pokemon);
+  }
+
+  selectPokemon(pokemon: Pokemon){
+    console.log(`Vous avez cliqué sur le pokémon ${pokemon.name}`);
+  }
 }
